4d2fd0fbed46f6caf5fa51fbd8b2e094a62e67c8
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-12-11 02:08:44 +0300

    structures child routes

--- Sanctuary-UI/src/app/Modules/structures/structures-routing.module.ts
+++ Sanctuary-UI/src/app/Modules/structures/structures-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccommondationsComponent } from './Components/accommondations/accommondations.component';
+import { CultureCenterComponent } from './Components/culture-center/culture-center.component';
+import { HallComponent } from './Components/hall/hall.component';
+import { MainGateComponent } from './Components/main-gate/main-gate.component';
+import { ShadesComponent } from './Components/shades/shades.component';
+import { WrittingsComponent } from './Components/writtings/writtings.component';
 import { StructuresComponent } from './structures.component';
 
-const routes: Routes = [{ path: '', component: StructuresComponent }];
+const routes: Routes = [
+  { path: '', component:StructuresComponent},
+  {path: 'shades', component:ShadesComponent},
+  {path: 'hall', component:HallComponent},
+  {path: 'writtings', component:WrittingsComponent},
+  {path: 'main-gate', component:MainGateComponent},
+  {path: 'culture-center', component:CultureCenterComponent},
+  {path: 'accommondations', component:AccommondationsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
