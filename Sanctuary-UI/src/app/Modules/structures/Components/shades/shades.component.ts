83e12c79e3ccd37890ed192558218d08af083988
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-12-11 02:18:48 +0300

    shades structure

--- Sanctuary-UI/src/app/Modules/structures/Components/shades/shades.component.ts
+++ Sanctuary-UI/src/app/Modules/structures/Components/shades/shades.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shades',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShadesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
+
+  goBack() {
+    this.router.navigate(['structures'])
+  }
 
   ngOnInit(): void {
   }
