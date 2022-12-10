10058572336ea25ba22dc7b67c3de70169e56e5e
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-12-11 02:20:37 +0300

    special features structure chile

--- Sanctuary-UI/src/app/Modules/structures/Components/writtings/writtings.component.ts
+++ Sanctuary-UI/src/app/Modules/structures/Components/writtings/writtings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-writtings',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WrittingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
+
+  goBack() {
+    this.router.navigate(['structures'])
+  }
 
   ngOnInit(): void {
   }
