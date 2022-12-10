29ec6a7390d0a5366366c1997e51adc972111acc
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-12-11 02:17:18 +0300

    main gate structure feature

--- Sanctuary-UI/src/app/Modules/structures/Components/main-gate/main-gate.component.ts
+++ Sanctuary-UI/src/app/Modules/structures/Components/main-gate/main-gate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-gate',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainGateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
+
+  goBack(){
+    this.router.navigate(['structures']);
+  }
 
   ngOnInit(): void {
   }
