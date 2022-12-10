eccb7926241f2173b80f40388eccc70c7d441f5d
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-12-11 02:06:58 +0300

    structures navigation functions

--- Sanctuary-UI/src/app/Modules/structures/structures.component.ts
+++ Sanctuary-UI/src/app/Modules/structures/structures.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-structures',
@@ -7,7 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructuresComponent implements OnInit {
 
-  constructor() { }
+    //shades function
+  goShades() {
+    this.router.navigate(['structures/shades'])
+  }
+
+  // Hall navigation function
+  goHall() {
+    this.router.navigate(['structures/hall'])
+  }
+
+  // Culture centre function
+  goCulture() {
+    this.router.navigate(['structures/culture-center'])
+  }
+
+  // Main gate function
+  goGate() {
+    this.router.navigate(['structures/main-gate'])
+  }
+
+  //Accommondations function
+  goAccommondations() {
+    this.router.navigate(['structures/accommondations'])
+  }
+
+  // Writtings navigation function
+  goWrittings() {
+    this.router.navigate(['structures/writtings'])
+  }
+
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
   }
