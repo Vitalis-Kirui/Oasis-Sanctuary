18756927b41882127b4965ba3d4552f344e214c3
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-12-11 01:36:10 +0300

    creating routes for basic components

--- Sanctuary-UI/src/app/app-routing.module.ts
+++ Sanctuary-UI/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'about-us', component:AboutUsComponent},
+  {path: 'contact-us', component:ContactUsComponent},
+  {path: '**', component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
